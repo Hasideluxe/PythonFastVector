275c74fb2306b4300a00a832a1aaeca2477f43d1
Sebastian Hein <sbhein@gmail.com>
2020-09-26 21:13:34 +0200

    first commit

---
+++ fastvector/__init__.py
@@ -0,0 +1,5 @@
+from .computations import (cython_clip_vector, naive_cython_clip_vector,
+                           python_clip_vector)
+from .dtypes import (float32, float64, int8, int16, int32, int64, uint8,
+                     uint16, uint32, uint64)
+from .vector import VectorND
